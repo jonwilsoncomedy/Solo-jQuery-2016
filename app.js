a04d00144edf96aa658ce2fd99ccd8ab79df065e
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-01 15:11:59 -0500

    on-click events tested, working, no content yet

--- app.js
+++ app.js
@@ -5,6 +5,28 @@ var yellow = 0;
 var green = 0;
 var blue = 0;
 
+$('body').on('click', '#red1', function() {
+
+
+});
+
+$('body').on('click', '#yellow1', function() {
+
+
+});
+
+$('body').on('click', '#green1', function() {
+
+
+});
+
+$('body').on('click', '#blue1', function() {
+
+
+});
+
+
+
 
 
 
